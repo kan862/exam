7dd4b0a94d1d9465c71a90dcdd07fa554caa8d7d
kan862 <kanchannaregal@gmail.com>
2025-06-10 13:10:51 +0530

    Update swapint.java

--- swapint.java
+++ swapint.java
@@ -5,7 +5,7 @@ public class swapint{
         System.out.println("a value:"+a);
         System.out.println("b value:"+b);
         a=b;
-        b=temp;
+        b=a;
         temp=a;
         System.out.println("after swapping");
         System.out.println("swapped number 1:" +temp);
