88227ebb218af6ebe33853cf616c74a0defefb2a
kan862 <kanchannaregal@gmail.com>
2025-06-10 12:56:22 +0530

    Update swapint.java

--- swapint.java
+++ swapint.java
@@ -1,9 +1,13 @@
 public class swapint{
     public static void main(String[] args){
         int a=2,b=6,temp;
+        System.out.println("before swapping");
+        System.out.println("a value:"+a);
+        System.out.println("b value:"+b);
         a=b;
         b=0;
         temp=a;
+        System.out.println("after swapping");
         System.out.println("swapped number is:" +temp);
        
     }
