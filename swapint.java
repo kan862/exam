3bba0fc5ef77e674133c4a26dbe9c199f9633bfa
Shakatiprasad007 <amoghajoshi2005@gmail.com>
2025-06-10 12:06:32 +0530

    save the file

---
+++ swapint.java
@@ -0,0 +1,10 @@
+public class swapint{
+    public static void main(String[] args){
+        int a=2,b=6,temp;
+        a=b;
+        b=0;
+        temp=a;
+        System.out.println("swapped number is:" +temp);
+       
+    }
+}
